ae26e6d2c476c856da3aa8e41764851fb800cc74
Sebastien Bürky <buerkyb@gmail.com>
2023-10-31 20:12:06 +0100

    minor changes

--- Data/PokemonService.cs
+++ Data/PokemonService.cs
@@ -15,6 +15,20 @@ namespace WildLifeApp.Data
         }
 
 
+        public Task<Pokemon> GetPokemon(string id)
+        {
+            return context.Pokedex
+            .Include(p => p.Abilities).Include(p => p.Type1).Include(p => p.Type2).FirstOrDefaultAsync(m => m.ID == id);
+        }
+
+        public Task<List<Learnset>> GetLearnset(Pokemon pkmn)
+        {
+            return Task.FromResult(context.Learnsets.Include(p => p.mon).Include(m => m.move).ThenInclude(mv => mv.type)
+            .Include(m => m.move).ThenInclude(mv => mv.MoveClass).Where(m => m.mon == pkmn).ToList());
+
+        }
+
+
 
     }
 }
